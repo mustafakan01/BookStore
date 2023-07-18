3adb17457df74cb8f9dcb67f669182b41ba49783
Mustafa Kan <79532179+mustafakan01@users.noreply.github.com>
2023-07-18 23:07:49 +0300

    final

--- BookStore/WebApi/Controllers/BookController.cs
+++ BookStore/WebApi/Controllers/BookController.cs
@@ -94,5 +94,25 @@ namespace WebApi.Controllers{
 
         }
 
+
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteBook(int id )
+        {
+
+            var book=BookList.SingleOrDefault(x=>x.Id==id);
+
+            if(book is null)
+            return BadRequest();
+
+
+
+            BookList.Remove(book);
+            return Ok();
+
+
+        }
+
     }
 }
